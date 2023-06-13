f5fae8b8fd019cdf2667787b9ee9fc784e5ecdbe
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-13 14:20:44 +0400

    Implement routes and components for client-side navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
-import Calculator from './components/Calculator';
+import Home from './routes/Home';
+import Quote from './routes/Quote';
+import Calculate from './routes/Calculate';
+import NoMatch from './routes/NoMatch';
+import Layout from './components/Layout';
 
 function App() {
-  return <Calculator />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="quote" element={<Quote />} />
+        <Route path="calculate" element={<Calculate />} />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
