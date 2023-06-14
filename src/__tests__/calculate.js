7c56b77919345d4cb3a521369422014b86825f47
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-14 11:58:35 +0400

    Implemnt test for calculate functionality

---
+++ src/__tests__/calculate.js
@@ -0,0 +1,23 @@
+import calculate from '../logic/calculate';
+
+describe('calculate function', () => {
+  test("returns initial state when 'AC' button is clicked", () => {
+    const result = calculate({ total: '5', next: '10', operation: '+' }, 'AC');
+    expect(result).toEqual({ total: null, next: null, operation: null });
+  });
+
+  test('handles decimal point correctly when "." button is clicked', () => {
+    const result = calculate({ total: null, next: '5', operation: null }, '.');
+    expect(result).toEqual({ total: null, next: '5.', operation: null });
+  });
+
+  test('performs addition when "+" button is clicked', () => {
+    const result = calculate({ total: '10', next: '5', operation: '+' }, '+');
+    expect(result).toEqual({ total: '15', next: null, operation: '+' });
+  });
+
+  test('performs subraction when "-" button is clicked', () => {
+    const result = calculate({ total: '10', next: '5', operation: '-' }, '-');
+    expect(result).toEqual({ total: '5', next: null, operation: '-' });
+  });
+});
