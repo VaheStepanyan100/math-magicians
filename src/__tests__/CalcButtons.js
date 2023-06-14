2da34051b39fb4e92bd501897282e9245f2b08d6
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-14 17:14:52 +0400

    Add tests to simulate user interaction

--- src/__tests__/CalcButtons.js
+++ src/__tests__/CalcButtons.js
@@ -1,10 +1,14 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import CalcButtons from '../components/CalcButtons';
 
 test('renders operator buttons correctly', () => {
   const handleButtonClick = jest.fn();
-  const { container } = render(
+  const { container, getByText } = render(
     <CalcButtons handleButtonClick={handleButtonClick} />,
   );
+  const button = getByText("AC");
+
+  fireEvent.click(button);
+  expect(handleButtonClick).toHaveBeenCalledTimes(1);
   expect(container).toMatchSnapshot();
 });
